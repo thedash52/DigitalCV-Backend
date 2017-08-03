171b39c60334c7c078cbb3a495f098846b259d37
thedash52 <thedash52@hotmail.com>
2017-08-03 18:26:27 +1200

    database connection and test set up

--- index.js
+++ index.js
@@ -1,9 +1,65 @@
 var express = require('express');
+var mysql = require('mysql');
+var Promise = require('promise');
 
 var app = express();
+const port = 3000;
 
-app.get('/', function(req, res) {
-    
+var pool = mysql.createPool({
+    connectionLimit: 100,
+    host: 'localhost',
+    user: 'root',
+    password: 'tHedAshc379sq',
+    database: 'digitalcv',
+    debug: true
 });
 
-app.listen(3000);
+function checkDatabase() {
+    return new Promise(function(resolve, reject) {
+        pool.getConnection((err, conn) => {
+            if (err) {
+                return reject(err);
+            }
+
+            conn.query("SELECT * FROM basic", function(err, result) {
+                conn.release();
+
+                if (!err) {
+                    return resolve(result);
+                }
+            });
+
+            conn.once('error', function(err) {
+                return reject(err);
+            });
+        });
+    });
+}
+
+app.get('/', function (req, res) {
+    res.status(200).json("Nothing to see here!");
+});
+
+app.get('/check-connection', function (req, res) {
+    var testResults = {
+        connection: true,
+        database: false,
+        err: ""
+    };
+
+    checkDatabase().then((result) => {
+        testResults.database = true;
+        res.status(200).json(testResults);
+    }).catch((err) => {
+        testResults.err = err;
+        res.status(200).json(testResults);
+    });
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('Connection Error', err);
+    }
+
+    console.log(`server is listening on ${port}`);
+});
